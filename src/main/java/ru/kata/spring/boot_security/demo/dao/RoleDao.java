704b46136cc17bbffe9d834a814d63c352681af5
Grigoriy-F <grinfed11@gmail.com>
2024-06-06 13:25:50 +0300

    Test

---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDao.java
@@ -0,0 +1,11 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+
+import ru.kata.spring.boot_security.demo.model.Role;
+
+public interface RoleDao {
+
+    Role findByRole (String role);
+
+    void save (Role role);
+}
